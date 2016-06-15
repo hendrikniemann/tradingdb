f0e66a86e310deedc2316f00a0b80783673b3206
hendrikniemann <h.neverman@web.de>
2016-06-15 23:25:23 +0200

    add model exports to models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
-import UserModel from './user/UserModel';
-import ItemModel from './item/ItemModel';
+import UserModel from './UserModel';
+import ItemModel from './ItemModel';
 
 UserModel.hasMany(ItemModel);
 ItemModel.belongsTo(UserModel);
+
+export { UserModel, ItemModel };
