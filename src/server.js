cf1f3790d676be169a15446a2cde769cdd0398d5
hendrikniemann <h.neverman@web.de>
2016-06-15 23:18:56 +0200

    create src directory and move models
    Models will be contained in src/models/ and types will be contained in
    src/types/.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import koa from 'koa';
 import graphqlMiddleware from 'koa-graphql';
 import mount from 'koa-mount';
+import bodyParser from 'koa-bodyparser';
 
 import './schema/types/associations';
 import Schema from './schema';
 
 const app = koa();
 
-app.use(mount('/graphql', graphqlMiddleware({
+app.use(bodyParser());
+
+app.use(mount('/graphql', graphqlMiddleware((_, context) => ({
   schema: Schema,
   graphiql: true,
-})));
+  context,
+}))));
 
 app.listen(3000);
