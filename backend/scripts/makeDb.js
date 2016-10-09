09e42df66ed80845723d1aa10d69bd91a2122308
hendrikniemann <h.neverman@web.de>
2016-10-09 20:33:09 +0200

    add some data to db creation script

--- backend/scripts/makeDb.js
+++ backend/scripts/makeDb.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-console */
 import connection from '../src/models/connection';
+import { UserModel, ItemModel } from '../src/models/';
 
-import '../src/models/';
-
+console.log('Create tables...');
 connection.sync({ force: true })
-  .then(() => console.log('Successfully created DB from Schema'))
+  .then(() => {
+    console.log('Successfully created tables.');
+    console.log('Filling in data...');
+    UserModel.create({
+      email: 'hendrik@tradingdb.de',
+      password: 'test',
+      items: [
+        {
+          description: 'WK 99+14+21',
+          bought: 120000000,
+          sold: 140000000,
+          soldOn: Date.now(),
+        },
+        {
+          description: '45er Bow r7+8 cl',
+          bought: 16000000,
+        },
+      ],
+    }, {
+      include: [{
+        model: ItemModel,
+        as: 'items',
+      }],
+    }).then(() => console.log('Data created.'));
+  })
   .catch(error => { throw error; });
