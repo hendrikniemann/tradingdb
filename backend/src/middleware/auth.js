7a31421f108152ed26dda9dbbed291c9c2bdbf3e
hendrikniemann <h.neverman@web.de>
2017-04-12 01:26:51 +0200

    create default user login for auth middleware
    When the application is run in development the auth middleware provides
    a mock user state without reading the JWT.

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,11 +1,18 @@
 /* @flow */
 import { verify } from './util/jwtutils';
 
-export default async function auth(ctx: any) {
+export default async function auth(ctx: any, next: () => Promise<any>) {
   const token = ctx.cookies.get('jwt');
   try {
     ctx.state.user = await verify(token).then(payload => payload.user);
   } catch (error) {
-    ctx.state.user = null;
+    ctx.state.logger.debug('[middleware/auth]: User login attempt with invalid token');
+    if (process.env.NODE_ENV === 'development') {
+      ctx.state.logger.debug('[middleware/auth]: Setting default user id 1 for this session');
+      ctx.state.user = 1;
+    } else {
+      ctx.throw(401, { message: 'The provided JWT is not valid.' });
+    }
   }
+  await next();
 }
