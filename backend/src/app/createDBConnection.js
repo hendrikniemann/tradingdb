e3a4271a273ce89b1f8cf0cede6dc46d40516b96
hendrikniemann <h.neverman@web.de>
2017-04-15 04:04:02 +0200

    Remove useless createDatabase function

--- backend/src/app/createDBConnection.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-import { connect, type ConnectionOptions, type Connection } from 'rethinkdb';
-
-export default function createDBConnection(config: ConnectionOptions): Promise<Connection> {
-  return connect(config);
-}
